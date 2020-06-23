3fc5e8e4951aea54af6773f6814b07ea29b75659
SathwikSharma226 <53095162+SathwikSharma226@users.noreply.github.com>
2020-06-23 23:04:45 +0530

    Add files via upload

---
+++ Week1/01-hello-world.playground/Pages/Untitled Page.xcplaygroundpage/Contents.swift
@@ -0,0 +1,19 @@
+import UIKit
+
+/*:
+ ### Example
+ */
+var str = "Hello, playground"
+
+var message = "Hello, World!"
+print(message)
+
+message = "Sathwik N Sharma"
+print(message)
+
+/*:
+ ### Exercise
+ 
+ 1.  Create a new variable ( var ) that stores your name
+ 2.  Print the variable
+ */
